8aa5c49bc7df04db54d2eb5554df850edca5bfb6
mdshakil20 <shakilislam737@gmail.com>
2022-10-12 09:42:43 +0600

    Correct Answer Toast with eye icon added

--- src/Compoents/Question/Question.js
+++ src/Compoents/Question/Question.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Question = (props) => {
     const { question, options, correctAnswer } = props.question;
@@ -15,18 +16,28 @@ const Question = (props) => {
             toast("Wrong Answer!");
         }
     }
-    console.log(props.question);
+    const ans = () => {
+
+    }
+
     return (
 
         <div className='p-3 my-5 '>
-            <p className=''>{question.replace(/(<([^>]+)>)/ig, '')}</p>
-            <div className="p-2 grid lg:grid-cols-2 md:lg:grid-cols-2 gap-3 text-left">
+            <div className='flex justify-between w-full'>
+                <p className='text-xl my-2'>{question.replace(/(<([^>]+)>)/ig, '')}</p>
+                <EyeIcon onClick={() => toast(correctAnswer)} className='w-5 cursor-pointer mr-3' ></EyeIcon>
+            </div>
+            <div className="text-xl p-2 grid lg:grid-cols-2 md:lg:grid-cols-2 gap-3 text-left">
                 {
                     options.map(option =>
                         <div className='p-2 text-left bg-slate-800 p-3 rounded-lg hover:bg-slate-600'>
                             <input onClick={(e) => handleGetValue(e.target.value)} type="radio" name="option" value={option} id={option} />
                             <label className='ml-4 text-xs md:text-base' htmlFor={option}>{option}</label>
-                            <ToastContainer  autoClose={2000}/>
+                            <ToastContainer
+                                position="top-right"
+                                autoClose={3000}
+                                theme="dark"
+                            />
                         </div>
                     )
                 }
--- src/Compoents/QuizDetails/QuizDetails.js
+++ src/Compoents/QuizDetails/QuizDetails.js
@@ -8,7 +8,7 @@ const QuizDetails = () => {
     return (
         <div>
             <h1 className='text-3xl text-center pb-3 border-b border-green-400 '>Quiz for {quiz.data.name}</h1>
-            <h3> new; {quiz.data.questions.length}</h3>
+            
             {
                 quiz.data.questions.map(Squestion => <Question
                     key={Squestion.id}
